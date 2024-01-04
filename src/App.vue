49ea11727eafc7e8c467f4ff6c23afea99c0e631
敖律风 <x123456789fy@outlook.com>
2024-01-04 20:04:26 +0800

    重新初始化项目

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h3>学习Vue</h3>
+  <p>{{ msg }}</p>
+  <p>{{ hello }}</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      msg: 'Hello Vue!',
+      hello: 'Hello World!'
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+h3 {
+  color: red;
 }
 </style>
