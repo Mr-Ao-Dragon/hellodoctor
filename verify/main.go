0d8b400ad2350e32203db38bfa559961973f4328
敖律风 <x123456789fy@outlook.com>
2024-03-05 12:05:57 +0800

    fix: 修正为微信要求的数据类型

--- verify/main.go
+++ verify/main.go
@@ -4,9 +4,12 @@ import (
 	"context"
 	"crypto/sha1"
 	"encoding/hex"
+	"encoding/json"
 	"log"
+	"net/http"
 	"os"
 	"sort"
+	"strconv"
 	"strings"
 
 	"github.com/aliyun/fc-runtime-go-sdk/fc"
@@ -20,6 +23,14 @@ type queryParameters struct {
 }
 type StructEvent struct {
 	QueryParameters queryParameters `json:"queryParameters"`
+}
+type ReposeBody struct {
+	StatusCode int16   `json:"statusCode"`
+	Body       int     `json:"body"`
+	Headers    headers `json:"headers"`
+}
+type headers struct {
+	ContentType string `json:"Content-Type"`
 }
 
 func HandleHttpRequest(ctx context.Context, event StructEvent) (repose string, err error) {
@@ -41,7 +52,12 @@ func HandleHttpRequest(ctx context.Context, event StructEvent) (repose string, e
 		log.Printf("Signature is correct!")
 		log.Printf("remote Signture is: %s", event.QueryParameters.Signature)
 		log.Printf("local Signature is: %s", localSignature)
-		repose = event.QueryParameters.EchoStr
+		reposeStr := new(ReposeBody)
+		reposeStr.StatusCode = http.StatusOK
+		reposeStr.Body, _ = strconv.Atoi(event.QueryParameters.EchoStr)
+		reposeStr.Headers.ContentType = "text/plain"
+		reposeByte, _ := json.Marshal(*reposeStr)
+		repose = string(reposeByte)
 		err = nil
 		log.Printf("repose: %s", repose)
 		return
@@ -49,6 +65,13 @@ func HandleHttpRequest(ctx context.Context, event StructEvent) (repose string, e
 		log.Printf("Signature is not correct")
 		log.Printf("remote Signture is: %s", event.QueryParameters.Signature)
 		log.Printf("local Signature is: %s", localSignature)
+		reposeStr := new(ReposeBody)
+		reposeStr.StatusCode = http.StatusBadRequest
+		reposeStr.Body = 0
+		reposeStr.Headers.ContentType = "text/plain"
+		reposeByte, _ := json.Marshal(*reposeStr)
+		repose = string(reposeByte)
+		err = nil
 		repose = ""
 		err = nil
 		return
