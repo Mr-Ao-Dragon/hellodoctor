50f9f921a525a8194cdf90122d5447bb3d0dda31
敖律风 <x123456789fy@outlook.com>
2024-03-30 21:09:41 +0800

    fix: 修复错误的 param key
    Signed-off-by: 敖律风 <x123456789fy@outlook.com>

--- user/login/main.go
+++ user/login/main.go
@@ -27,9 +27,9 @@ type ReposeBody struct {
 func HandleHttpRequest(ctx context.Context, event datastruct.EventStruct) (repose *datastruct.UniversalRepose, err error) {
 	// Convert code to OpenID
 	repose = new(datastruct.UniversalRepose)
-	OpenID, err := user.CodeToOpenID(event.QueryParameters["code"].(string))
+	OpenID, err := user.CodeToOpenID(event.QueryParameters["token"].(string))
 	if err != nil {
-		log.Printf("Query Failed By: %s", event.QueryParameters["code"])
+		log.Printf("Query Failed By: %s", event.QueryParameters["token"])
 		repose.StatusCode = http.StatusUnauthorized
 		repose.Headers.ContentType = ContentType.JsonUTF8
 		repose.Headers.AccessControlAllowOrigin = "*"
