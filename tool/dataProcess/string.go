417891499f8c7cf7e46b2c00362020cc014a5e67
敖律风 <x123456789fy@outlook.com>
2024-04-02 23:15:05 +0800

    add: 添加读取 Authorization 功能

--- tool/dataProcess/string.go
+++ tool/dataProcess/string.go
@@ -13,3 +13,11 @@ func CutOpenID(Raw string) (string, error) {
 	}
 	return Raw[index+1:], nil
 }
+func readToken(Raw map[string]any) (string, error) {
+	index := strings.IndexByte(Raw["Authorization"].(string), ' ')
+	if index == -1 {
+		err := errors.New("token Not found")
+		return "", err
+	}
+	return Raw["Authorization"].(string)[index+1:], nil
+}
